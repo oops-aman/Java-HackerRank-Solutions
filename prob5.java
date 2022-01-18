39820d4186b51b03e05054edc7da7e2b03d3a5c4
oops-aman <b200054@nitsikkim.ac.in>
2022-01-19 00:07:07 +0530

    Java Output Formatting

--- prob5.java
+++ prob5.java
@@ -8,7 +8,7 @@ public class prob5 {
         for (int i = 0; i < 3; i++) {
             String s1 = sc.next();
             int x = sc.nextInt();
-            // Complete this line
+            System.out.printf("%-15s%03d\n", s1, x);
         }
         System.out.println("================================");
         sc.close();
